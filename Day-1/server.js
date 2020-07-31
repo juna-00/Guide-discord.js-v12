d5d608d143dd5d9acd699adc519a3385e3aba5bd
juna-00 <68531972+juna-00@users.noreply.github.com>
2020-07-31 14:08:48 +0700

    Update server.js

--- Day-1/server.js
+++ Day-1/server.js
@@ -3,14 +3,14 @@ const client = new Discord.Client(); //Creating Discord new client
 
 client.once('ready', () => { //When bot is ready , bot will run given code
 	console.log('Ready!'); //Sends Ready to console as message
-  client.user.setActivity(`I am Devil`); //Sets bot activity as "I am Devil" 
+  client.user.setActivity(`Nusantara Indonesia Roleplay`); //Sets bot activity as "I am Devil" 
 });
 
 client.on("message", message => { //when Someone message 
-  if(message.content === "!ping") { //if message is same as !ping
+  if(message.content === "J!ping") { //if message is same as !ping
     return message.channel.send(`Pong ${client.ws.ping}`); //it will return message
   }
 });
 
-client.login('your-token-goes-here'); //Paste Your Bot Token
+client.login('NzM3OTI1ODYxODUxNzkxMzkw.XyEdAw.yRuH5b1kYGfvLdKSn_9Y51oh-_w'); //Paste Your Bot Token
 
