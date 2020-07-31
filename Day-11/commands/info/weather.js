e4eebafa15383e9ce9c74e07b29d3598a8f32841
juna-00 <68531972+juna-00@users.noreply.github.com>
2020-07-31 14:03:28 +0700

    Update weather.js

--- Day-11/commands/info/weather.js
+++ Day-11/commands/info/weather.js
@@ -10,7 +10,7 @@ module.exports = {
     
     
     if(!args.length) {
-      return message.channel.send("Please give the weather location")
+      return message.channel.send("Tolong beri lokasi nama")
     }
     
  weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
@@ -19,13 +19,13 @@ try {
 let embed = new discord.MessageEmbed()
 .setTitle(`Weather - ${result[0].location.name}`)
 .setColor("#ff2050")
-.setDescription("Temperature units can may be differ some time")
-.addField("Temperature", `${result[0].current.temperature} Celcius`, true)
-.addField("Sky Text", result[0].current.skytext, true)
-.addField("Humidity", result[0].current.humidity, true)
-.addField("Wind Speed", result[0].current.windspeed, true)//What about image
-.addField("Observation Time", result[0].current.observationtime, true)
-.addField("Wind Display", result[0].current.winddisplay, true)
+.setDescription("Unit suhu dapat berbeda waktu")
+.addField("Suhu", `${result[0].current.temperature} Celcius`, true)
+.addField("Text Langit", result[0].current.skytext, true)
+.addField("Kelembaban", result[0].current.humidity, true)
+.addField("Kecepatan Angin", result[0].current.windspeed, true)//What about image
+.addField("Waktu Pengamat", result[0].current.observationtime, true)
+.addField("Layar Angin", result[0].current.winddisplay, true)
 .setThumbnail(result[0].current.imageUrl);
    message.channel.send(embed)
 } catch(err) {
